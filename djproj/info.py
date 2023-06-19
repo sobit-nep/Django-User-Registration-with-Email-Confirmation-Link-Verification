23633ffbaefb187093056d43ff634c389e17a355
sobit-nep <nepalsobit1@gmail.com>
2023-06-19 22:53:53 +0545

    Files added

---
+++ djproj/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER= 'jd884317@gmail.com'
+EMAIL_HOST_PASSWORD= 'apwjtxejndwylrxj' #password generated from security page(search for "App Password") of that email 
+EMAIL_PORT = 587
